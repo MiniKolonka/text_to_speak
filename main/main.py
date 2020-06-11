c090267feccc597e3668f06942541a999ead3671
Mini_Kolonka <53175704+MiniKolonka@users.noreply.github.com>
2020-06-11 12:58:48 +0300

    Update main.py
    correction of minor errors

--- main/main.py
+++ main/main.py
@@ -1,56 +1,45 @@
-# Перевод текста в голос и *.mp3
-
-# pip install gTTS
-from gtts import gTTS as speaker
-
-# Модуль для полушение битрейта видео
 from io import BytesIO
+from gtts import gTTS
 
 
-class Text_to_speak():
-    """Инструменты для спикинга"""
-
-    # Инициализируем класс
-    def __init__(self, text, lang):
+class TextToSpeak():
+    def __init__(self, text: str, lang: str) -> None:
         self.text = text
         self.lang = lang
 
-    
-    def to_mp3(self, _file = 'speak.mp3'):
-        """Преводим текст в *.mp3"""
-        tts = speaker(f'{self.text}', lang=f'{self.lang}')
-        tts.save(f'{_file}')
-        print(f"Создался файл {_file}") 
+    def to_mp3(self, filename: str = "speak") -> None:
+        """convert text to .mp3"""
+        tts = gTTS(self.text, lang=self.lang)
+        tts.save(f"{filename}.mp3")
 
-    
-    def to_play(self):
-        """Проигрываем наш текст"""
+    def play(self) -> None:
+        """plays the text"""
         mp3_fp = BytesIO()
-        tts = speaker(f'{self.text}', lang=f'{self.lang}')
+
+        tts = gTTS(self.text, lang=self.lang)
         tts.write_to_fp(mp3_fp)
 
-
-    def streams(self):
-        """Получаем потоки"""
-        tts = speaker(f'{self.text}', lang=f'{self.lang}')
+    def stream(self) -> str:
+        """Receives stream"""
+        tts = gTTS(self.text, lang=self.lang)
         urls = tts.get_urls()
-        return urls
-    
-    def stream(self):
-        """Получаем поток"""
-        tts = speaker(f'{self.text}', lang=f'{self.lang}')
-        urls = tts.get_urls()
-        url = urls[0]
-        return url
 
-    
-    def mp3_to_text(self, _file):
-        """Считываем файл и получаем текст"""
-        tts = speaker(f'{self.text}', lang=f'{self.lang}')
-        with open(f'{_file}', 'wb') as f:
-            tts.write_to_fp(f)
+        return urls[0]
+
+    def mp3_to_text(self, filename: str) -> None:
+        """Reads a file and receives text"""
+        tts = gTTS(self.text, lang=self.lang)
+
+        with open(filename, 'wb') as file:
+            tts.write_to_fp(file)
 
 
+def main() -> None:
+    text = input("Введите такст: ")
+    lang = input("Введите язык: ")
+    speaker = TextToSpeak(text, lang)
+    speaker.to_mp3()
 
 
-
+if __name__ == "__main__":
+    main()
